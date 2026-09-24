ee2f3a18aec4af531a48240acfd7a50a7558b1d0
DSandy11 <sandhyadornal11gmail.com>
2026-09-24 12:34:49 +0530

    Added deafult values for email and groups

--- utils/constants/appConstants.ts
+++ utils/constants/appConstants.ts
@@ -1,9 +1,15 @@
 export const SUCCESS = "success";
 export const ERROR = "error";
 
+// Entra ID's default SAML claim URIs for email and group claims.
 export const TENANT_FORM_INITIAL_VALUES = {
   liscencePackage: "basic",
-  sso: { emailAttribute: "email", groupsAttribute: "groups" },
+  sso: {
+    emailAttribute:
+      "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
+    groupsAttribute:
+      "http://schemas.microsoft.com/ws/2008/06/identity/claims/groups",
+  },
 };
 
 export const DEFAULT_PAGE_SIZE = 10;
